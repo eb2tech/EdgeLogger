aa22aead14df8d30a7592ffadc96a29e2a825637
Edward Blakes <eblakes@eb2technologies.com>
2026-02-02 16:21:23 -0600

    Write to LiteDB

--- EdgeLogger.ApiService/Services/AuraLogMessageHandler.cs
+++ EdgeLogger.ApiService/Services/AuraLogMessageHandler.cs
@@ -1,9 +1,34 @@
-﻿using NATS.Net;
+﻿using System.Text.Json.Serialization;
+using LiteDB;
+using NATS.Net;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace EdgeLogger.ApiService.Services;
 
 public class AuraLogMessageHandler(NatsClient natsClient, ILogger<AuraLogMessageHandler> logger) : BackgroundService
 {
+    public string DatabasePath 
+    {
+        get
+        {
+            var basePath = Environment.GetEnvironmentVariable("EDGELOGGER_DATA_DIR")
+                           ?? BuildAndEnsureDirectoryPath();
+
+            var dbPath = Path.Combine(basePath, "edgeLogger.db");
+            return dbPath;
+
+            static string BuildAndEnsureDirectoryPath()
+            {
+                var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "EdgeLogger");
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                return path;
+            }
+        }
+    }
+
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         await foreach (var message in natsClient.SubscribeAsync<string>("aura2.logs.>", cancellationToken: stoppingToken))
@@ -11,8 +36,10 @@ public class AuraLogMessageHandler(NatsClient natsClient, ILogger<AuraLogMessage
             try
             {
                 var deviceName = message.Subject.Split(".")[^1];
-                var logMessage = message.Data;
-                logger.LogInformation("Received Aura log message from {DeviceName}: {LogMessage}", deviceName, logMessage);
+                var logMessage = JsonSerializer.Deserialize<AuraLogMessage>(message.Data!);
+                logger.LogDebug("Received Aura log message from {DeviceName}: {LogMessage}", deviceName, logMessage);
+
+                WriteLogMessage(deviceName, logMessage);
             }
             catch (Exception ex)
             {
@@ -20,4 +47,33 @@ public class AuraLogMessageHandler(NatsClient natsClient, ILogger<AuraLogMessage
             }
         }
     }
+
+    private void WriteLogMessage(string deviceName, AuraLogMessage? logMessage)
+    {
+        if (logMessage is null) return;
+
+        var logStore = new AuraLogStore(
+            Timestamp: DateTimeOffset.FromUnixTimeSeconds(logMessage.Timestamp).UtcDateTime,
+            Message: logMessage.Message,
+            DeviceName: deviceName
+        );
+
+        using var db = new LiteDatabase(DatabasePath);
+        var collection = db.GetCollection<AuraLogStore>("AuraLogs");
+        collection.EnsureIndex(x => x.DeviceName);
+
+        collection.Insert(logStore);
+    }
 }
+
+public record AuraLogMessage(
+    [property: JsonPropertyName("timestamp")] long Timestamp,
+    [property: JsonPropertyName("message")] string Message,
+    [property: JsonPropertyName("message_length")] int MessageLen
+);
+
+public record AuraLogStore(
+    DateTime Timestamp,
+    string Message,
+    string DeviceName
+);
